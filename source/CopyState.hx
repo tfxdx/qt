48d5d371f4d10f8491d1ab06bd321304ad13cdd6
tfxdx <yunumendozaperez@gmail.com>
2026-03-21 20:36:45 -0500

    Update CopyState.hx

--- source/CopyState.hx
+++ source/CopyState.hx
@@ -37,7 +37,7 @@ class CopyState extends MusicBeatState
 		checkExistingFiles();
 		if (maxLoopTimes <= 0)
 		{
-			FlxG.switchState(new TitleState());
+			FlxG.switchState(new TitleState_BACKUP());
 			return;
 		}
 
@@ -87,7 +87,7 @@ class CopyState extends MusicBeatState
 				}
 				canUpdate = false;
 				FlxG.sound.play(Paths.sound('confirmMenu')).onComplete = () -> {
-					FlxG.switchState(new TitleState());
+					FlxG.switchState(new TitleState_BACKUP());
 				};
 			}
 
