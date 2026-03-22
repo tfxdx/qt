f0068560cfd6ff84de5efb08d4b9c7711c3184f4
tfxdx <yunumendozaperez@gmail.com>
2026-03-21 20:40:48 -0500

    Update CopyState.hx

--- source/CopyState.hx
+++ source/CopyState.hx
@@ -47,7 +47,7 @@ class CopyState extends MusicBeatState
 
 		add(new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, 0xffcaff4d));
 
-		loadingImage = new FlxSprite(0, 0, Paths.image('MenuDesat'));
+		loadingImage = new FlxSprite(0, 0, Paths.image('menuDesat'));
 		loadingImage.setGraphicSize(0, FlxG.height);
 		loadingImage.updateHitbox();
 		loadingImage.screenCenter();
