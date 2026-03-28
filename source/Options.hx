76f8404a646953e4eb6a83b4be0abda62c9b1eeb
tfxdx <yunumendozaperez@gmail.com>
2026-03-28 13:36:23 -0500

    Update Options.hx

--- source/Options.hx
+++ source/Options.hx
@@ -510,6 +510,23 @@ class OffsetMenu extends Option
 		return "Time your offset";
 	}
 }
+class MobileControls extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.switchState(new options.CustomControlsState());
+		return true;
+	}
+	private override function updateDisplay():String
+	{
+		return "Mobile Controls";
+	}
 
+}
 
 
